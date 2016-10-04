d0f344bdeed5559e3405a99ccb22f3eb095aac84
Andrea Coravos <acoravos@gmail.com>
2016-10-04 15:18:36 -0400

    Update chapter02.scala

--- chapter02.scala
+++ chapter02.scala
@@ -1,7 +1,7 @@
 package com.acoravos.scala.ch02
 
 /**
- * Write a Scala equivalent for the Java loop
+ * QUESTION 4: Write a Scala equivalent for the Java loop
  * for (int i = 10; i >= 0; i--) System.out.println(i);
  */
 
@@ -18,11 +18,47 @@ object Ex04 extends App {
 
 }
 
-// using a method
+/**
+ * QUESTION 5: Write a procedure countdown(n: Int)
+ * that prints the numbers from n to 0;
+ */
 
-def countdown(n: Int) = {
+object Ex05 extends App {
+
+  def countdown(n: Int) = {
 	for( i <- Range(n, 0, -1) )
 		println(i)
 }
 
 countdown(10);
+
+/**
+ * QUESTION 6: Write a for loop for computing the product
+ * of the Unicode codes of all letters in a string;
+ */
+
+object Ex06 extends App {
+
+  def unicodeproduct(str: String) = {
+	var result: Long = 1;
+	for( characters <- str ) result *= characters.toLong
+	result
+}
+
+println(unicodeproduct("Hello"));
+
+/**
+ * QUESTION 7: Solve the preceding exercise wihtout writing
+ * a loop. Hint: Look at the StringOps ScalaDoc;
+ */
+
+object Ex07 extends App {
+
+  // 1L is a long (twos complement integer) of 64 bits
+  "Hello".foldLeft(1L)((a, b) => a * b)
+
+}
+
+
+
+}
