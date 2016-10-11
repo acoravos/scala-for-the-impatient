cccecf4520a89c4fdc8e7b98b6b47523a6c9047e
Andrea Coravos <acoravos@gmail.com>
2016-10-11 09:47:55 -0400

    Update chapter03.scala

--- chapter03.scala
+++ chapter03.scala
@@ -1,6 +1,12 @@
 // Q1: Write a code snippet that sets a to an array
 // of n random integers between 0 (inclusive) and n (exclusive)
+import util.Random
 
 var a = new Array[Int](10)
-for(i <- 0 until a.length) a(i) = scala.util.Random.nextInt(10)
-a   
+for(i <- 0 until a.length) a(i) = Random.nextInt(10)
+a 
+
+// Q2: Write a loop that swaps adjacent elements of an array of
+// integers. For example, Array (1,2,3,4,5) becomes Array(2,1,4,3,5)
+
+for (elem <- arr) 
